575053dbed0ec494f457fba3383b3aa2aba7f4a4
hduobao <1713471338@qq.com>
2024-06-02 00:09:51 +0800

    feat: The login and register function was initialized,and basic response struct in used

--- app/user/cmd/main.go
+++ app/user/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"Goim-server/app/user/internal/database"
 	"Goim-server/app/user/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	database.InitMySqlDB()
 	// 创建一个默认的 Gin 路由器
 	router := gin.Default()
 	routes.SetupRoutes(router)
